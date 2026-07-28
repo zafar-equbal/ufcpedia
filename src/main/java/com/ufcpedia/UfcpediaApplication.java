fa7e16d0a7d616b932438c52b47a2d8c0164763c
Md Zafar Equbal <zequbal705@gmail.com>
2026-07-29 01:42:04 +0530

    Initial Spring Boot project setup by z

---
+++ src/main/java/com/ufcpedia/UfcpediaApplication.java
@@ -0,0 +1,13 @@
+package com.ufcpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcpediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcpediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufcpedia/UfcpediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcpedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcpediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
